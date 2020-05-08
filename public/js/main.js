58fc8b60879ba7a0c4f2e17f2588488ec30a78e2
Tingan Sob <tingansob@gmail.com>
2020-05-08 11:37:49 -0400

    redesign - again.

---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+const sum=(l,o)=>l+o;console.log("This is a dynamically linked file."),console.log("The sum of 5 + 3 = "+sum(5,3)),console.log("Hello Kevin");
--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,4 @@
 const sum = (a, b) => a + b;
 console.log('This is a dynamically linked file.');
 console.log('The sum of 5 + 3 = ' + sum(5, 3));
+console.log('Hello Kevin');
